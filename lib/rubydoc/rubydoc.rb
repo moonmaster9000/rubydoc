663095df3f218edc49245da8a536982f11466291
Matt Parker <moonmaster9000@gmail.com>
2011-01-09 08:56:22 -0500

    complete

--- lib/rubydoc/rubydoc.rb
+++ lib/rubydoc/rubydoc.rb
@@ -2,7 +2,11 @@ class RubyDoc
   SITE    = "http://rubydoc.info"
   PREFIX  = "gems"
   
-  def self.url(gem, version=nil)
-    [SITE, PREFIX, gem, version, "frames"].compact.join "/" 
+  def self.url(gem, version=nil, no_frames=nil)
+    url = [SITE, PREFIX, gem, version].compact.join "/" 
+    if gem and !no_frames
+      url += "/frames"
+    end
+    url
   end
 end
